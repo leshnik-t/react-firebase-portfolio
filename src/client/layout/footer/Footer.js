39be7a8d330794fc6fa8664151ab920e7cb7bf6d
leshnik-t <lenatl@gmail.com>
2023-07-23 09:39:51 +0300

    hide login link

--- src/client/layout/footer/Footer.js
+++ src/client/layout/footer/Footer.js
@@ -1,7 +1,6 @@
 import Wrapper from '../../components/wrapper/Wrapper';
 import SocialLinks from '../../components/sociallinks/SocialLinks';
 import './footer.css';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
     const today = new Date();
@@ -17,18 +16,6 @@ const Footer = () => {
                     <div className="row">
                         <div className="col text-center mb-5">
                             <p>&copy; {today.getFullYear()} All rights reserved.</p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col text-center mb-3">
-                            <Link
-                                to="/login"
-                                className="btn btn-primary nav-link-small"
-                                title="Login"
-                                role="button"
-                            >
-                                Login
-                            </Link>
                         </div>
                     </div>
                 </div>
